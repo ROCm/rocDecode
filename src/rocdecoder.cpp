ca36b50794b02028e4f5a258658b9827120e5055
Aryan <aryan.salmanpour@amd.com>
2023-07-28 15:26:13 -0400

    add initial support for the base rocdecode class

--- src/rocdecoder.cpp
+++ src/rocdecoder.cpp
@@ -19,3 +19,337 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
+#include "rocdecoder.hpp"
+
+ROCDecode::ROCDecode(int device_id) : num_devices_{0}, device_id_{device_id}, hip_stream_ {0},
+    external_mem_handle_desc_{{}}, external_mem_buffer_desc_{{}}, va_display_{nullptr},
+    va_profiles_{{}}, num_va_profiles_{-1}, yuv_dev_mem_{nullptr}, width_{0}, height_{0},
+    chroma_height_{0}, surface_height_{0}, surface_width_{0}, num_chroma_planes_{0},
+    surface_stride_{0}, surface_size_{0}, bit_depth_{8}, byte_per_pixel_{1},
+    subsampling_{ROCDEC_FMT_YUV420}, out_image_info_{{}}, fp_out_{nullptr}, drm_fd_{-1} {
+
+    if (!InitHIP(device_id_)) {
+        std::cerr << "Failed to initilize the HIP" << std::endl;
+        throw std::runtime_error("Failed to initilize the HIP");
+    }
+    InitDRMnodes();
+    InitVAAPI();
+}
+
+ROCDecode::~ROCDecode() {
+    if (hip_stream_) {
+        hipError_t hip_status = hipSuccess;
+        hip_status = hipStreamDestroy(hip_stream_);
+        if (hip_status != hipSuccess) {
+            std::cout << "ERROR: hipStream_Destroy failed! (" << hip_status << ")" << std::endl;
+        }
+    }
+    if (drm_fd_ != -1) {
+        close(drm_fd_);
+    }
+    if (va_display_) {
+        vaTerminate(va_display_);
+    }
+}
+
+bool ROCDecode::Decode(uint8_t *data, size_t size, int64_t pts) {
+    //TODO implement the Decode function
+    return false;
+}
+
+uint8_t *ROCDecode::GetFrame(int64_t *pts) {
+    //TODO implement the GetFrame function
+    return nullptr;
+}
+
+bool ROCDecode::ReleaseFrame(int64_t pts) {
+    //TODO implement the ReleaseFrame function
+    return false;
+}
+
+void ROCDecode::SaveImage(std::string output_file_name, void *dev_mem, OutputImageInfo *image_info, bool is_output_RGB) {
+    uint8_t *hst_ptr = nullptr;
+    uint64_t output_image_size = image_info->output_image_size_in_bytes;
+    if (hst_ptr == nullptr) {
+        hst_ptr = new uint8_t [output_image_size];
+    }
+    hipError_t hip_status = hipSuccess;
+    hip_status = hipMemcpyDtoH((void *)hst_ptr, dev_mem, output_image_size);
+    if (hip_status != hipSuccess) {
+        std::cout << "ERROR: hipMemcpyDtoH failed! (" << hip_status << ")" << std::endl;
+        delete [] hst_ptr;
+        return;
+    }
+
+    // no RGB dump if the surface type is YUV400
+    if (image_info->chroma_format == ROCDEC_FMT_YUV400 && is_output_RGB) {
+        return;
+    }
+    uint8_t *tmp_hst_ptr = hst_ptr;
+    if (fp_out_ == nullptr) {
+        fp_out_ = fopen(output_file_name.c_str(), "wb");
+    }
+    if (fp_out_) {
+        int img_width = image_info->output_width;
+        int img_height = image_info->output_height;
+        int output_image_stride =  image_info->output_h_stride;
+        if (img_width * image_info->bytes_per_pixel == output_image_stride && img_height == image_info->output_v_stride) {
+            fwrite(hst_ptr, 1, output_image_size, fp_out_);
+        } else {
+            uint32_t width = is_output_RGB ? image_info->output_width * 3 : image_info->output_width;
+            if (image_info->bit_depth == 8) {
+                for (int i = 0; i < image_info->output_height; i++) {
+                    fwrite(tmp_hst_ptr, 1, width, fp_out_);
+                    tmp_hst_ptr += output_image_stride;
+                }
+                if (!is_output_RGB) {
+                    // dump chroma
+                    uint8_t *uv_hst_ptr = hst_ptr + output_image_stride * image_info->output_v_stride;
+                    for (int i = 0; i < img_height >> 1; i++) {
+                        fwrite(uv_hst_ptr, 1, width, fp_out_);
+                        uv_hst_ptr += output_image_stride;
+                    }
+                }
+            } else if (image_info->bit_depth > 8 &&  image_info->bit_depth <= 16 ) {
+                for (int i = 0; i < img_height; i++) {
+                    fwrite(tmp_hst_ptr, 1, width * image_info->bytes_per_pixel, fp_out_);
+                    tmp_hst_ptr += output_image_stride;
+                }
+                if (!is_output_RGB) {
+                    // dump chroma
+                    uint8_t *uv_hst_ptr = hst_ptr + output_image_stride * image_info->output_v_stride;
+                    for (int i = 0; i < img_height >> 1; i++) {
+                        fwrite(uv_hst_ptr, 1, width * image_info->bytes_per_pixel, fp_out_);
+                        uv_hst_ptr += output_image_stride;
+                    }
+                }
+            }
+        }
+    }
+
+    if (hst_ptr != nullptr) {
+        delete [] hst_ptr;
+        hst_ptr = nullptr;
+        tmp_hst_ptr = nullptr;
+    }
+}
+
+void ROCDecode::GetDeviceinfo(std::string &device_name, std::string &gcn_arch_name, int &pci_bus_id, int &pci_domain_id, int &pci_device_id, std::string &drm_node) {
+    device_name = hip_dev_prop_.name;
+    gcn_arch_name = hip_dev_prop_.gcnArchName;
+    pci_bus_id = hip_dev_prop_.pciBusID;
+    pci_domain_id = hip_dev_prop_.pciDomainID;
+    pci_device_id = hip_dev_prop_.pciDeviceID;
+    drm_node = drm_nodes_[device_id_];
+}
+
+std::string ROCDecode::GetPixFmtName(RocDecImageFormat subsampling) {
+    std::string fmt_name = "";
+    switch (subsampling) {
+        case ROCDEC_FMT_YUV420:
+            fmt_name = "YUV420";
+            break;
+        case ROCDEC_FMT_YUV444:
+            fmt_name = "YUV444";
+            break;
+        case ROCDEC_FMT_YUV422:
+            fmt_name = "YUV422";
+            break;
+        case ROCDEC_FMT_YUV400:
+            fmt_name = "YUV400";
+            break;
+       case ROCDEC_FMT_YUV420P10:
+            fmt_name = "YUV420P10";
+            break;
+       case ROCDEC_FMT_YUV420P12:
+            fmt_name = "YUV420P12";
+            break;
+        case ROCDEC_FMT_RGB:
+            fmt_name = "RGB";
+            break;
+        default:
+            std::cerr << "ERROR: subsampling format is not supported!" << std::endl;
+    }
+    return fmt_name;
+}
+
+std::string ROCDecode::GetCodecFmtName(rocDecVideoCodec codec_id) {
+    std::string fmt_name = "";
+    switch (codec_id) {
+        case rocDecVideoCodec_MPEG1:
+            fmt_name = "MPEG1";
+            break;
+        case rocDecVideoCodec_MPEG2:
+            fmt_name = "MPEG1";
+            break;
+        case rocDecVideoCodec_MPEG4:
+            fmt_name = "MPEG4";
+            break;
+       case rocDecVideoCodec_H264:
+            fmt_name = "H264";
+            break;
+       case rocDecVideoCodec_HEVC:
+            fmt_name = "HEVC";
+            break;
+        case rocDecVideoCodec_VP8:
+            fmt_name = "VP8";
+            break;
+        case rocDecVideoCodec_VP9:
+            fmt_name = "VP9";
+            break;
+        case rocDecVideoCodec_JPEG:
+            fmt_name = "JPEG";
+            break;
+        case rocDecVideoCodec_AV1:
+            fmt_name = "AV1";
+            break;
+        default:
+            std::cerr << "ERROR: subsampling format is not supported!" << std::endl;
+    }
+    return fmt_name;
+}
+
+bool ROCDecode::GetOutputImageInfo(OutputImageInfo **image_info) {
+    if (!width_ || !height_) {
+        std::cerr << "ERROR: ROCDecoder is not intialized" << std::endl;
+        return false;
+    }
+    *image_info = &out_image_info_;
+    return true;
+}
+
+bool ROCDecode::InitHIP(int device_id)
+{
+    hipError_t hip_status = hipSuccess;
+    hip_status = hipGetDeviceCount(&num_devices_);
+    if (hip_status != hipSuccess) {
+        std::cerr << "ERROR: hipGetDeviceCount failed! (" << hip_status << ")" << std::endl;
+        return false;
+    }
+    if (num_devices_ < 1) {
+        std::cerr << "ERROR: didn't find any GPU!" << std::endl;
+        return false;
+    }
+    if (device_id >= num_devices_) {
+        std::cerr << "ERROR: the requested device_id is not found! " << std::endl;
+        return false;
+    }
+    hip_status = hipSetDevice(device_id);
+    if (hip_status != hipSuccess) {
+        std::cerr << "ERROR: hipSetDevice(" << device_id << ") failed! (" << hip_status << ")" << std::endl;
+        return false;
+    }
+    hip_status = hipGetDeviceProperties(&hip_dev_prop_, device_id);
+    if (hip_status != hipSuccess) {
+        std::cerr << "ERROR: hipGetDeviceProperties for device (" << device_id << ") failed! (" << hip_status << ")" << std::endl;
+        return false;
+    }
+    hip_status = hipStreamCreate(&hip_stream_);
+    if (hip_status != hipSuccess) {
+        std::cerr << "ERROR: hipStream_Create failed! (" << hip_status << ")" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void ROCDecode::InitDRMnodes() {
+    // build the DRM render node names
+    for (int i = 0; i < num_devices_; i++) {
+        drm_nodes_.push_back("/dev/dri/renderD" + std::to_string(128 + i));
+    }
+}
+
+bool ROCDecode::InitVAAPI() {
+    if (drm_nodes_.size() == 0) {
+        std::cerr << "VAAPI initialization failed!" << std::endl;
+        return false;
+    }
+    drm_fd_ = open(drm_nodes_[device_id_].c_str(), O_RDWR);
+    if (drm_fd_ < 0) {
+        std::cerr << "ERROR: failed to open drm node " << drm_nodes_[device_id_].c_str() << std::endl;
+        return false;
+    }
+    va_display_ = vaGetDisplayDRM(drm_fd_);
+    if (!va_display_) {
+        std::cerr << "error: vaGetDisplayDRM failed! " << std::endl;
+        return false;
+    }
+
+    vaSetInfoCallback(va_display_, NULL, NULL);
+
+    VAStatus va_status;
+    int major_version = 0, minor_version = 0;
+    va_status = vaInitialize(va_display_, &major_version, &minor_version);
+    if (va_status != VA_STATUS_SUCCESS) {
+        std::cerr << "ERROR: vaInitialize failed! " << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool ROCDecode::QueryVaProfiles() {
+    if (va_display_ == nullptr) {
+        if (!InitVAAPI()) {
+            return false;
+        }
+    }
+    if (num_va_profiles_ > -1) {
+        //already queried available profiles
+        return true;
+    }
+    num_va_profiles_ = vaMaxNumProfiles(va_display_);
+    VAStatus va_status;
+    va_status = vaQueryConfigProfiles(va_display_, va_profiles_, &num_va_profiles_);
+    if (va_status != VA_STATUS_SUCCESS) {
+        std::cerr << "ERROR: vaQueryConfigProfiles failed! " << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool ROCDecode::GetImageSizeHintInternal(RocDecImageFormat subsampling, const uint32_t width, const uint32_t height, uint32_t *output_stride, size_t *output_image_size) {
+    if ( output_stride == nullptr || output_image_size == nullptr) {
+        std::cerr << "invalid input parameters!" << std::endl;
+        return false;
+    }
+    int aligned_height = 0;
+    switch (subsampling) {
+        case ROCDEC_FMT_YUV420:
+            *output_stride = align(width, 256);
+            aligned_height = align(height, 16);
+            *output_image_size = *output_stride * (aligned_height + (aligned_height >> 1));
+             break;
+        case ROCDEC_FMT_YUV444:
+            *output_stride = align(width, 256);
+            aligned_height = align(height, 16);
+            *output_image_size = *output_stride * aligned_height * 3;
+            break;
+        case ROCDEC_FMT_YUV400:
+            *output_stride = align(width, 256);
+            aligned_height = align(height, 16);
+            *output_image_size = *output_stride * aligned_height;
+            break;
+        case ROCDEC_FMT_RGB:
+            *output_stride = align(width, 256) * 3;
+            aligned_height = align(height, 16);
+            *output_image_size = *output_stride * aligned_height;
+            break;
+        case ROCDEC_FMT_YUV420P10:
+            *output_stride = align(width, 128) * 2;
+            aligned_height = align(height, 16);
+            *output_image_size = *output_stride * (aligned_height + (aligned_height >> 1));
+            break;
+        case ROCDEC_FMT_YUV420P12:
+            *output_stride = align(width, 128) * 2;
+            aligned_height = align(height, 16);
+            *output_image_size = *output_stride * (aligned_height + (aligned_height >> 1));
+            break;
+        default:
+            std::cerr << "ERROR: "<< GetPixFmtName(subsampling) <<" is not supported! " << std::endl;
+            return false;
+    }
+
+    return true;
+
+}
--- src/rocdecoder.hpp
+++ src/rocdecoder.hpp
@@ -22,12 +22,115 @@ THE SOFTWARE.
 
 #pragma once
 
+#include <unistd.h>
+#include <cstring>
+#include <iomanip>
 #include <assert.h>
 #include <stdint.h>
+#include <fcntl.h>
+#include <map>
 #include <vector>
-#include <string>
+#include <queue>
+#include <mutex>
 #include <iostream>
 #include <sstream>
-#include <string.h>
+#include <string>
+#include <va/va.h>
+#include <va/va_drmcommon.h>
+#include <va/va_drm.h>
+#include <hip/hip_runtime.h>
 #include "../api/rocdecode.h"
-#include <map>
+
+#define MAX_VA_PROFILES 36
+
+static inline int align(int value, int alignment) {
+   return (value + alignment - 1) & ~(alignment - 1);
+}
+
+typedef struct DecFrameBufferType {
+    VASurfaceID va_surface_id;                              /**< VASurfaceID for the decoded frame buffer */
+    hipExternalMemory_t hip_ext_mem;                        /**< interop hip memory for the decoded surface */
+    VADRMPRIMESurfaceDescriptor va_drm_prime_surface_desc;  /**< DRM surface descriptor */
+} DecFrameBuffer;
+
+typedef enum {
+    ROCDEC_FMT_YUV420 = 0,
+    ROCDEC_FMT_YUV444 = 1,
+    ROCDEC_FMT_YUV422 = 2,
+    ROCDEC_FMT_YUV400 = 3,
+    ROCDEC_FMT_YUV420P10 = 4,
+    ROCDEC_FMT_YUV420P12 = 5,
+    ROCDEC_FMT_RGB = 6,
+    ROCDEC_FMT_MAX = 7
+} RocDecImageFormat;
+
+typedef struct OutputImageInfoType {
+    uint32_t output_width;               /**< Output width of decoded image*/
+    uint32_t output_height;              /**< Output height of decoded image*/
+    uint32_t output_h_stride;            /**< Output horizontal stride in bytes of luma plane, chroma hstride can be inferred based on chromaFormat*/
+    uint32_t output_v_stride;            /**< Output vertical stride in number of columns of luma plane,  chroma vstride can be inferred based on chromaFormat */
+    uint32_t bytes_per_pixel;            /**< Output BytesPerPixel of decoded image*/
+    uint32_t bit_depth;                  /**< Output BitDepth of the image*/
+    uint64_t output_image_size_in_bytes; /**< Output Image Size in Bytes; including both luma and chroma planes*/ 
+    RocDecImageFormat chroma_format;      /**< Chroma format of the decoded image*/
+} OutputImageInfo;
+
+class ROCDecode {
+    public:
+       ROCDecode(int device_id = 0);
+       ~ROCDecode();
+       bool Decode(uint8_t *data, size_t size, int64_t pts = 0);
+       uint8_t* GetFrame(int64_t *pts);
+       bool ReleaseFrame(int64_t pts);
+       void SaveImage(std::string output_file_name, void* dev_mem, OutputImageInfo* image_info, bool is_output_RGB = 0);
+       void GetDeviceinfo(std::string &device_name, std::string &gcn_arch_name, int &pci_bus_id, int &pci_domain_id, int &pci_device_id, std::string &drm_node);
+       std::string GetPixFmtName(RocDecImageFormat subsampling);
+       uint32_t GetWidth() { assert(width_); return width_;}
+       uint32_t GetHeight() { assert(height_); return height_; }
+       uint32_t GetBitDepth() { assert(bit_depth_); return bit_depth_; }
+       uint32_t GetBytePerPixel() { assert(byte_per_pixel_); return byte_per_pixel_; }
+       size_t GetSurfaceSize() {assert(surface_size_); return surface_size_; }
+       uint32_t GetSurfaceStride() {assert(surface_stride_); return surface_stride_; }
+       RocDecImageFormat GetSubsampling() { return subsampling_; }
+       int GetSurfaceWidth() {assert(surface_width_); return surface_width_;}
+       int GetSurfaceHeight() {assert(surface_height_); return surface_height_;}
+       std::string GetCodecFmtName(rocDecVideoCodec codec_id);
+       bool GetOutputImageInfo(OutputImageInfo **image_info);
+
+    private:
+       bool InitHIP(int device_id);
+       void InitDRMnodes();
+       bool InitVAAPI();
+       bool QueryVaProfiles();
+       bool GetImageSizeHintInternal(RocDecImageFormat subsampling, const uint32_t output_width, const uint32_t output_height,
+            uint32_t *output_stride, size_t *output_image_size);
+       int num_devices_;
+       int device_id_;
+       hipDeviceProp_t hip_dev_prop_;
+       hipStream_t hip_stream_;
+       hipExternalMemoryHandleDesc external_mem_handle_desc_;
+       hipExternalMemoryBufferDesc external_mem_buffer_desc_;
+       VADisplay va_display_;
+       VAProfile va_profiles_[MAX_VA_PROFILES];
+       int num_va_profiles_;
+       void *yuv_dev_mem_;
+       uint32_t width_;
+       uint32_t height_;
+       uint32_t chroma_height_;
+       uint32_t surface_height_;
+       uint32_t surface_width_;
+       uint32_t num_chroma_planes_;
+       uint32_t num_components_;
+       uint32_t surface_stride_;
+       size_t surface_size_;
+       uint32_t bit_depth_;
+       uint32_t byte_per_pixel_;
+       RocDecImageFormat subsampling_;
+       OutputImageInfo out_image_info_;
+       std::vector<std::string> drm_nodes_;
+       std::mutex mutex_;
+       std::queue<DecFrameBuffer *> dec_frame_q_;
+       std::vector<VASurfaceID> dec_buffer_pool_;
+       FILE *fp_out_;
+       int drm_fd_;
+};
