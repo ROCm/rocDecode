e82ae0b75caa7d9d075a2f0c739d8b78f816d263
kiritigowda <kiriti.nageshgowda@amd.com>
2023-10-24 13:28:19 -0700

    Link Libraries - Updates & cleanup

--- cmake/FindLibva.cmake
+++ cmake/FindLibva.cmake
@@ -22,16 +22,28 @@
 ################################################################################
 
 find_library(LIBVA_LIBRARY NAMES va)
+find_library(LIBVA_DRM_LIBRARY NAMES va-drm)
 find_path(LIBVA_INCLUDE_DIR NAMES va/va.h)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Libva DEFAULT_MSG LIBVA_INCLUDE_DIR LIBVA_LIBRARY)
-mark_as_advanced(LIBVA_INCLUDE_DIR LIBVA_LIBRARY)
+mark_as_advanced(LIBVA_INCLUDE_DIR LIBVA_LIBRARY LIBVA_DRM_LIBRARY)
 
 if(Libva_FOUND)
   if(NOT TARGET Libva::va)
     add_library(Libva::va UNKNOWN IMPORTED)
     set_target_properties(Libva::va PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LIBVA_INCLUDE_DIR}"
         IMPORTED_LOCATION "${LIBVA_LIBRARY}")
+  endif()
+  if(NOT TARGET Libva::va_drm)
+    add_library(Libva::va_drm UNKNOWN IMPORTED)
+    set_target_properties(Libva::va_drm PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${LIBVA_INCLUDE_DIR}"
+      IMPORTED_LOCATION "${LIBVA_DRM_LIBRARY}")
+  endif()
+  message("-- ${White}Using Libva -- \n\tLibraries:${LIBVA_LIBRARY} \n\tIncludes:${LIBVA_INCLUDE_DIR}${ColourReset}")
+  message("-- ${White}Using Libva-drm -- \n\tLibraries:${LIBVA_DRM_LIBRARY}${ColourReset}")
+else()
+  if(Libva_FIND_REQUIRED)
+    message(FATAL_ERROR "{Red}FindLibva -- Libva NOT FOUND${ColourReset}")
   endif()
 endif()
