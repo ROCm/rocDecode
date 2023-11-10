71fca7d51c77ec5bc30235d0c7e97e18aa399fad
LakshmiKumar23 <lakshmi.kumar@amd.com>
2023-11-10 07:26:10 -0800

    review comments

--- samples/videoDecodeFork/videodecodefork.cpp
+++ samples/videoDecodeFork/videodecodefork.cpp
@@ -38,29 +38,19 @@ THE SOFTWARE.
 #include "video_demuxer.hpp"
 #include "roc_video_dec.h"
 
-static int *nTotal;
+static int *n_total;
 
-void DecProc(RocVideoDecoder *pDec, VideoDemuxer *demuxer, int *pnFrame) {
-    int nVideoBytes = 0, nFrameReturned = 0, nFrame = 0;
-    uint8_t *pVideo = nullptr;
-    uint8_t *pFrame = nullptr;
+void DecProc(RocVideoDecoder *p_dec, VideoDemuxer *demuxer, int *pn_frame) {
+    int n_video_bytes = 0, n_frame_returned = 0, n_frame = 0;
+    uint8_t *p_video = nullptr;
     int64_t pts = 0;
-    double totalDecTime = 0.0;
-    auto startTime = std::chrono::high_resolution_clock::now();
 
     do {
-        demuxer->Demux(&pVideo, &nVideoBytes, &pts);
-        nFrameReturned = pDec->DecodeFrame(pVideo, nVideoBytes, 0, pts);
-        nFrame += nFrameReturned;
-    } while (nVideoBytes);
-
-    auto endTime = std::chrono::high_resolution_clock::now();
-    auto timePerFrame = std::chrono::duration<double, std::milli>(endTime - startTime).count();
-
-    // Calculate average decoding time
-    totalDecTime = timePerFrame;
-    double averageDecodingTime = totalDecTime / nFrame;
-    double nFPS = 1000 / averageDecodingTime;
+        demuxer->Demux(&p_video, &n_video_bytes, &pts);
+        n_frame_returned = p_dec->DecodeFrame(p_video, n_video_bytes, 0, pts);
+        n_frame += n_frame_returned;
+    } while (n_video_bytes);
+    *pn_frame = n_frame;
 }
 
 void ShowHelpAndExit(const char *option = NULL) {
@@ -72,9 +62,7 @@ void ShowHelpAndExit(const char *option = NULL) {
 
 int main(int argc, char **argv) {
 
-    std::string inputFilePath;
-    int isOutputRGB = 0;
-    int deviceId = 0;
+    std::string input_file_path;
     int n_fork = 4;
     Rect *p_crop_rect = nullptr;
     OUTPUT_SURF_MEMORY_TYPE mem_type = OUT_SURFACE_MEM_DEV_INTERNAL;        // set to internal
@@ -91,7 +79,7 @@ int main(int argc, char **argv) {
             if (++i == argc) {
                 ShowHelpAndExit("-i");
             }
-            inputFilePath = argv[i];
+            input_file_path = argv[i];
             continue;
         }
         if (!strcmp(argv[i], "-t")) {
@@ -107,81 +95,85 @@ int main(int argc, char **argv) {
         ShowHelpAndExit(argv[i]);
     }
 
-    std::vector<std::unique_ptr<VideoDemuxer>> vDemuxer;
-    std::vector<std::unique_ptr<RocVideoDecoder>> vViddec;
-    std::vector<int> vDeviceId(n_fork);
+    try {
+        std::vector<std::unique_ptr<VideoDemuxer>> v_demuxer;
+        std::vector<std::unique_ptr<RocVideoDecoder>> v_viddec;
+        std::vector<int> v_device_id(n_fork);
 
-    // TODO: Change this block to use VCN query API 
-    int numDevices = 0;
-    hipError_t hipStatus = hipSuccess;
-    hipStatus = hipGetDeviceCount(&numDevices);
-    if (hipStatus != hipSuccess) {
-        std::cout << "ERROR: hipGetDeviceCount failed! (" << hipStatus << ")" << std::endl;
-        return 1;
-    }
-
-    int sd = (numDevices >= 2) ? 0 : 0;
-
-    for (int i = 0; i < n_fork; i++) {
-        std::unique_ptr<VideoDemuxer> demuxer(new VideoDemuxer(inputFilePath.c_str()));
-        rocDecVideoCodec rocdec_codec_id = AVCodec2RocDecVideoCodec(demuxer->GetCodecID());
-        vDeviceId[i] = (i % 2 == 0) ? 0 : sd;
-        std::unique_ptr<RocVideoDecoder> dec(new RocVideoDecoder(vDeviceId[i], mem_type, rocdec_codec_id, false, true, p_crop_rect));
-        vDemuxer.push_back(std::move(demuxer));
-        vViddec.push_back(std::move(dec));
-    }
-
-    float totalFPS = 0;
-    std::vector<int> vFrame;
-    vFrame.resize(n_fork, 0);
-
-    std::string deviceName, gcnArchName, drmNode;
-    int pciBusID, pciDomainID, pciDeviceID;
-
-    for (int i = 0; i < n_fork; i++) {
-        vViddec[i]->GetDeviceinfo(deviceName, gcnArchName, pciBusID, pciDomainID, pciDeviceID);
-        std::cout << "info: stream " << i << " using GPU device " << vDeviceId[i] << " - " << deviceName << "[" << gcnArchName << "] on PCI bus " <<
-        std::setfill('0') << std::setw(2) << std::right << std::hex << pciBusID << ":" << std::setfill('0') << std::setw(2) <<
-        std::right << std::hex << pciDomainID << "." << pciDeviceID << std::dec << std::endl;
-        std::cout << "info: decoding started for thread " << i << " ,please wait!" << std::endl;
-    }
-
-    int pid_status;
-    pid_t pids[n_fork];
-    nTotal = (int *) mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    *nTotal = 0;
-
-    auto startTime = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < n_fork; i++) {
-        if ((pids[i] = fork()) < 0) {
-            std::cout << "ERROR: failed to create fork" << std::endl;
-            abort();
+        // TODO: Change this block to use VCN query API 
+        int num_devices = 0;
+        hipError_t hip_status = hipSuccess;
+        hip_status = hipGetDeviceCount(&num_devices);
+        if (hip_status != hipSuccess) {
+            std::cout << "ERROR: hipGetDeviceCount failed! (" << hip_status << ")" << std::endl;
+            return 1;
         }
-        else if (pids[i] == 0) {
-            DecProc(vViddec[i].get(), vDemuxer[i].get(), &vFrame[i]);
-            *nTotal += vFrame[i];
-            _exit(0);
+
+        int sd = (num_devices >= 2) ? 1 : 0;
+
+        for (int i = 0; i < n_fork; i++) {
+            std::unique_ptr<VideoDemuxer> demuxer(new VideoDemuxer(input_file_path.c_str()));
+            rocDecVideoCodec rocdec_codec_id = AVCodec2RocDecVideoCodec(demuxer->GetCodecID());
+            v_device_id[i] = (i % 2 == 0) ? 0 : sd;
+            std::unique_ptr<RocVideoDecoder> dec(new RocVideoDecoder(v_device_id[i], mem_type, rocdec_codec_id, false, true, p_crop_rect));
+            v_demuxer.push_back(std::move(demuxer));
+            v_viddec.push_back(std::move(dec));
         }
-    }
 
-    for(int i = 0; i < n_fork; i++) {	
-        waitpid(pids[i], &pid_status, 0);
-        if (!WIFEXITED(pid_status)) {
-            std::cout << "child with " << pids[i] << " exited with status " << WEXITSTATUS(pid_status) << std::endl;
+        std::vector<int> v_frame;
+        v_frame.resize(n_fork, 0);
+
+        std::string device_name, gcn_arch_name;
+        int pci_bus_id, pci_domain_id, pci_device_id;
+
+        for (int i = 0; i < n_fork; i++) {
+            v_viddec[i]->GetDeviceinfo(device_name, gcn_arch_name, pci_bus_id, pci_domain_id, pci_device_id);
+            std::cout << "info: stream " << i << " using GPU device " << v_device_id[i] << " - " << device_name << "[" << gcn_arch_name << "] on PCI bus " <<
+            std::setfill('0') << std::setw(2) << std::right << std::hex << pci_bus_id << ":" << std::setfill('0') << std::setw(2) <<
+            std::right << std::hex << pci_domain_id << "." << pci_device_id << std::dec << std::endl;
+            std::cout << "info: decoding started for thread " << i << " ,please wait!" << std::endl;
         }
+
+        int pid_status;
+        pid_t pids[n_fork];
+        n_total = (int *) mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        *n_total = 0;
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < n_fork; i++) {
+            if ((pids[i] = fork()) < 0) {
+                std::cout << "ERROR: failed to create fork" << std::endl;
+                abort();
+            }
+            else if (pids[i] == 0) {
+                DecProc(v_viddec[i].get(), v_demuxer[i].get(), &v_frame[i]);
+                *n_total += v_frame[i];
+                _exit(0);
+            }
+        }
+
+        for(int i = 0; i < n_fork; i++) {	
+            waitpid(pids[i], &pid_status, 0);
+            if (!WIFEXITED(pid_status)) {
+                std::cout << "child with " << pids[i] << " exited with status " << WEXITSTATUS(pid_status) << std::endl;
+            }
+        }
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration<double, std::milli>(end_time - start_time).count();
+
+        // Calculate average decoding time
+        double total_decoding_time = 0.0;
+        total_decoding_time = duration;
+        double average_decoding_time = total_decoding_time / *n_total;
+
+        std::cout << "info: Total Frames Decoded: " << *n_total << std::endl;
+        std::cout << "info: avg decoding time per frame (ms): " << average_decoding_time  << std::endl;
+        std::cout << "info: avg FPS: " << 1000 / average_decoding_time << std::endl;
+        munmap(n_total, sizeof(int));
+    } catch (const std::exception &ex) {
+      std::cout << ex.what() << std::endl;
+      exit(1);
     }
-    auto endTime = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration<double, std::milli>(endTime - startTime).count();
-
-    // Calculate average decoding time
-    double totalDecodingTime = 0.0;
-    totalDecodingTime = duration;
-    double averageDecodingTime = totalDecodingTime / *nTotal;
-
-    std::cout << "info: Total Frames Decoded: " << *nTotal << std::endl;
-    std::cout << "info: avg decoding time per frame (ms): " << averageDecodingTime  << std::endl;
-    std::cout << "info: avg FPS: " << 1000 / averageDecodingTime << std::endl;
-    munmap(nTotal, sizeof(int));
 
     return 0;
 }
