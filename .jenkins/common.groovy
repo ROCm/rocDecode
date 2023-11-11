2a351082d1cca5921270f3219d7ae746f49f8906
kiritigowda <kiritigowda@gmail.com>
2023-11-10 23:02:21 -0600

    Jenkins - Save all built packages

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -53,17 +53,32 @@ def runPackageCommand(platform, project) {
 
     String packageType = ""
     String packageInfo = ""
+    String osType = ''
 
     if (platform.jenkinsLabel.contains('centos') ||
         platform.jenkinsLabel.contains('rhel') ||
         platform.jenkinsLabel.contains('sles')) {
         packageType = 'rpm'
         packageInfo = 'rpm -qlp'
+        if(platform.jenkinsLabel.contains('sles')){
+            osType = 'sles'
+        }
+        else if(platform.jenkinsLabel.contains('rhel8')){
+            osType = 'rhel8'
+        }
+        else if(platform.jenkinsLabel.contains('rhel9')){
+            osType = 'rhel9'
         }
     else
     {
         packageType = 'deb'
         packageInfo = 'dpkg -c'
+        if(platform.jenkinsLabel.contains('ubuntu20')){
+            osType = 'ubuntu20'
+        }
+        else if(platform.jenkinsLabel.contains('ubuntu22')){
+            osType = 'ubuntu22'
+        }
     }
 
     def command = """#!/usr/bin/env bash
@@ -73,11 +88,11 @@ def runPackageCommand(platform, project) {
                 cd ${project.paths.project_build_prefix}/build/release
                 sudo make package
                 mkdir -p package
-                mv rocdecode-dev*.${packageType} package/
-                mv rocdecode_*.${packageType} package/
-                mv Testing/Temporary/*.log package/
-                ${packageInfo} package/rocdecode-dev*.${packageType}
-                ${packageInfo} package/rocdecode_*.${packageType}
+                mkdir -p package/${osType}
+                mv *.${packageType} package/${osType}/
+                mv Testing/Temporary/*.log package/${osType}/
+                ${packageInfo} package/${osType}/rocdecode-dev*.${packageType}
+                ${packageInfo} package/${osType}/rocdecode_0*.${packageType}
                 """
 
     platform.runCommand(this, command)
