257703d97263e747c44898a48c9643814233e5f1
rrawther <Rajy.MeeyakhanRawther@amd.com>
2023-11-30 09:51:55 -0800

    add copyright block

--- utils/colorspace_kernels.cpp
+++ utils/colorspace_kernels.cpp
@@ -1,11 +1,27 @@
 /*
-Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+Copyright (c) 2023 - 2023 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 
 #include "colorspace_kernels.h"
-
-#define HIP_ASSERT(status) \
- assert(status == hipSuccess)
+#include "roc_video_dec.h"
  
 __constant__ float matYuv2Rgb[3][3];
 __constant__ float matRgb2Yuv[3][3];
@@ -59,7 +75,7 @@ void SetMatYuv2Rgb(int colStandard) {
             mat[i][j] = (float)(1.0 * max / (white - black) * mat[i][j]);
         }
     }
-    HIP_ASSERT(hipMemcpyToSymbol(matYuv2Rgb, mat, sizeof(mat)));
+    HIP_API_CALL(hipMemcpyToSymbol(matYuv2Rgb, mat, sizeof(mat)));
 }
 
 void SetMatRgb2Yuv(int colStandard) {
@@ -76,7 +92,7 @@ void SetMatRgb2Yuv(int colStandard) {
             mat[i][j] = (float)(1.0 * (white - black) / max * mat[i][j]);
         }
     }
-    HIP_ASSERT(hipMemcpyToSymbol(matRgb2Yuv, mat, sizeof(mat)));
+    HIP_API_CALL(hipMemcpyToSymbol(matRgb2Yuv, mat, sizeof(mat)));
 }
 
 template<class T>
