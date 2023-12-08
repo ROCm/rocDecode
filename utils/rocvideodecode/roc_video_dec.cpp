23148ba353a8190597056a91ba64d1e7b69cce2f
rrawther <Rajy.MeeyakhanRawther@amd.com>
2023-12-07 16:40:18 -0800

    address review comments

--- utils/rocvideodecode/roc_video_dec.cpp
+++ utils/rocvideodecode/roc_video_dec.cpp
@@ -74,12 +74,16 @@ RocVideoDecoder::~RocVideoDecoder() {
 
     std::lock_guard<std::mutex> lock(mtx_vp_frame_);
     if (out_mem_type_ != OUT_SURFACE_MEM_DEV_INTERNAL) {
-        for (DecFrameBuffer p_frame : vp_frames_) {
+        for (auto p_frame : vp_frames_) {
             if (p_frame.frame_ptr) {
-              if (out_mem_type_ == OUT_SURFACE_MEM_DEV_COPIED)
-                  hipFree(p_frame.frame_ptr);
+              if (out_mem_type_ == OUT_SURFACE_MEM_DEV_COPIED) {
+                  hipError_t hip_status = hipFree(p_frame.frame_ptr);
+                  if (hip_status != hipSuccess) {
+                      std::cout << "ERROR: hipFree failed! (" << hip_status << ")" << std::endl;
+                  }
+              }
               else
-                  delete[] p_frame.frame_ptr;
+                  delete[] (p_frame.frame_ptr);
               p_frame.frame_ptr = nullptr;
             }
         }
