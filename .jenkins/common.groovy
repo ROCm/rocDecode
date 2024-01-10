ccda5e11aef7a97561d3368f62b7713cd58c7586
Kiriti Gowda <kiritigowda@gmail.com>
2024-01-09 21:08:28 -0800

    Update common.groovy

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -12,7 +12,7 @@ def runCompileCommand(platform, project, jobName, boolean debug=false, boolean s
         installDKMS = 'sudo yum -y install amdgpu-dkms'
     }
     else if (platform.jenkinsLabel.contains('sles')) {
-        installDKMS = 'sudo zypper -n install amdgpu-dkms'
+        installDKMS = 'echo amdgpu-dkms not available'
     }
 
     def command = """#!/usr/bin/env bash
@@ -106,8 +106,6 @@ def runPackageCommand(platform, project) {
                 mv rocdecode-dev*.${packageType} package/${osType}-rocdecode-dev.${packageType}
                 mv rocdecode-test*.${packageType} package/${osType}-rocdecode-test.${packageType}
                 mv ${packageRunTime}.${packageType} package/${osType}-rocdecode.${packageType}
-                mv Testing/Temporary/LastTest.log package/${osType}-LastTest.log
-                mv Testing/Temporary/LastTestsFailed.log package/${osType}-LastTestsFailed.log
                 ${packageDetail} package/${osType}-rocdecode-dev.${packageType}
                 ${packageDetail} package/${osType}-rocdecode-test.${packageType}
                 ${packageDetail} package/${osType}-rocdecode.${packageType}
