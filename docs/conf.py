efc0950041ba38f295c5446af74af997a27cefa8
kiritigowda <kiritigowda@gmail.com>
2024-02-06 23:08:55 -0800

    rocDecode Docs - Updates

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,13 @@ with open('../CMakeLists.txt', encoding='utf-8') as f:
 left_nav_title = f"rocDecode {version_number} Documentation"
 
 shutil.copy2('../README.md','./quickStart.md')
+shutil.copy2('../sample/README.md','./sample/README.md')
+shutil.copy2('../samples/videoDecode/README.md','./samples/videoDecode/README.md')
+shutil.copy2('../samples/videoDecodeFork/README.md','./samples/videoDecodeFork/README.md')
+shutil.copy2('../samples/videoDecodeMem/README.md','./samples/videoDecodeMem/README.md')
+shutil.copy2('../samples/videoDecodeMultiFiles/README.md','./samples/videoDecodeMultiFiles/README.md')
+shutil.copy2('../samples/videoDecodePerf/README.md','./samples/videoDecodePerf/README.md')
+shutil.copy2('../samples/videoDecodeRGB/README.md','./samples/videoDecodeRGB/README.md')
 
 # for PDF output on Read the Docs
 project = "rocDecode Documentation"
--- samples/README.md
+++ samples/README.md
@@ -1,4 +1,4 @@
-# Samples
+# Samples overview
 
 rocDecode samples
 
