25f3008d63d39afbdce7205962fda8e352bef7b3
Aryan <aryan.salmanpour@amd.com>
2024-03-26 10:57:23 -0400

    modify the condition of using visble_devices by making sure that the requsted device id is valid

--- src/rocdecode/vaapi/vaapi_videodecoder.cpp
+++ src/rocdecode/vaapi/vaapi_videodecoder.cpp
@@ -80,7 +80,7 @@ rocDecStatus VaapiVideoDecoder::InitializeDecoder(std::string gcn_arch_name) {
     std::vector<int> visible_devices;
     GetVisibleDevices(visible_devices);
     std::string drm_node;
-    if (visible_devices.size() != 0) {
+    if (decoder_create_info_.device_id < visible_devices.size()) {
         drm_node = "/dev/dri/renderD" + std::to_string(128 + visible_devices[decoder_create_info_.device_id] * num_render_cards_per_device);
     } else {
         drm_node = "/dev/dri/renderD" + std::to_string(128 + decoder_create_info_.device_id * num_render_cards_per_device);
