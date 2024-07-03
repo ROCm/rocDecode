24acdee08271d217f27de2ada7bb1168dbb35bd3
paveltc <Pavel.Tcherniaev@amd.com>
2024-07-03 12:12:35 -0700

    changed if statement to check n_frame_returned

--- samples/videoDecodeBatch/videodecodebatch.cpp
+++ samples/videoDecodeBatch/videodecodebatch.cpp
@@ -125,8 +125,8 @@ void DecProc(RocVideoDecoder *p_dec, VideoDemuxer *demuxer, int *pn_frame, doubl
         n_frame_returned = p_dec->DecodeFrame(p_video, n_video_bytes, 0, pts);
         n_frame += n_frame_returned;
         if (b_dump_output_frames && mem_type != OUT_SURFACE_MEM_NOT_MAPPED) {
-            if(n_frame){
-                if(!p_dec->GetOutputSurfaceInfo(&surf_info)){
+            if (n_frame_returned) {
+                if (!p_dec->GetOutputSurfaceInfo(&surf_info)) {
                     std::cerr << "Error: Failed to get Output Surface Info!" << std::endl;
                     break;
                 }
