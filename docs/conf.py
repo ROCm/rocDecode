eef1bdc46226193efe9f49b860f81a34d75576eb
Aryan Salmanpour <aryan.salmanpour@amd.com>
2025-01-02 10:07:54 -0500

    update year to 2025

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ left_nav_title = f"rocDecode {version_number} documentation"
 # for PDF output on Read the Docs
 project = "rocDecode documentation"
 author = "Advanced Micro Devices, Inc."
-copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+copyright = "Copyright (c) 2024 - 2025 Advanced Micro Devices, Inc. All rights reserved."
 version = version_number
 release = version_number
 
