049e7eaa0dfbd8d51300eb184dd8d4b66a5dcc8a
Kiriti Gowda <kiritigowda@gmail.com>
2025-03-19 14:28:23 -0500

    Update common.groovy

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -132,9 +132,9 @@ def runPackageCommand(platform, project) {
                 ${packageInfo} package/${osType}-rocdecode-dev.${packageType}
                 ${packageInfo} package/${osType}-rocdecode-test.${packageType}
                 ${packageInfo} package/${osType}-rocdecode.${packageType}
-                ${packageInstall} package/${osType}-rocdecode.${packageType}
-                ${packageInstall} package/${osType}-rocdecode-dev.${packageType}
-                ${packageInstall} package/${osType}-rocdecode-test.${packageType}
+                sudo ${packageInstall} package/${osType}-rocdecode.${packageType}
+                sudo ${packageInstall} package/${osType}-rocdecode-dev.${packageType}
+                sudo ${packageInstall} package/${osType}-rocdecode-test.${packageType}
                 """
 
     platform.runCommand(this, command)
