b8d3c3225be07dbe20ab9ecc7039da4f1e674b62
LakshmiKumar23 <lakshmi.kumar@amd.com>
2025-03-19 15:26:39 -0700

    remove warning

--- src/amd_detail/rocdecode_api_trace.cpp
+++ src/amd_detail/rocdecode_api_trace.cpp
@@ -91,7 +91,7 @@ template <typename Tp> void ToolInit(Tp* table) {
 #if ROCDECODE_ROCPROFILER_REGISTER > 0
     auto table_array = std::array<void*, 1>{static_cast<void*>(table)};
     auto lib_id = rocprofiler_register_library_indentifier_t{};
-    auto rocp_reg_status = rocprofiler_register_library_api_table(
+    rocprofiler_register_library_api_table(
         dispatch_table_info<Tp>::name, dispatch_table_info<Tp>::import_func,
         dispatch_table_info<Tp>::version, table_array.data(), table_array.size(), &lib_id);
 #else
