0669eef25b559228c5b2d97446a5b7e43c5034b1
kiritigowda <kiritigowda@gmail.com>
2025-03-27 14:30:59 -0700

    Jenkins - Fix

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -42,6 +42,7 @@ def runTestCommand (platform, project) {
                 export HOME=/home/jenkins
                 ${libvaDriverPath}
                 echo make test
+                export LLVM_PROFILE_FILE=\"${project.paths.project_build_prefix}/rawdata/rocdecode-%p.profraw\"
                 cd ${project.paths.project_build_prefix}/build/release
                 LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/rocm/lib${libLocation} make test ARGS="-VV --rerun-failed --output-on-failure"
                 echo rocdecode-sample - videoDecode
@@ -68,6 +69,7 @@ def runTestCommand (platform, project) {
                 cmake /opt/rocm/share/rocdecode/samples/videoDecodePerf/
                 make -j8
                 LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/rocm/lib${libLocation} ./videodecodeperf -i ./../data1.img
+                echo $(pwd)
                 cd  ../../../
                 llvm-profdata merge -sparse rawdata/*.profraw -o rocdecode.profdata
                 llvm-cov export -object /build/release/lib/librocdecode.so --instr-profile=rocdecode.profdata --format=lcov > coverage.info
