30ad38c1f67ea5b633c2cd5050556f8eed2e4aa6
rrawther <Rajy.MeeyakhanRawther@amd.com>
2025-05-16 10:54:22 -0700

    resolved review comments

--- src/rocdecode-host/rocdecode_host_api.cpp
+++ src/rocdecode-host/rocdecode_host_api.cpp
@@ -23,7 +23,6 @@ THE SOFTWARE.
 #include "../api/rocdecode/rocdecode.h"
 #include "../src/commons.h"
 
-    //namespace rocdecodehost {
 /*****************************************************************************************************/
 //! \fn rocDecStatus ROCDECAPI rocDecCreateDecoderHost(rocDecDecoderHandle *decoder_handle, RocDecoderCreateInfoHost *decoder_create_info)
 //! Create the decoder object based on decoder_create_info. A handle to the created decoder is returned
@@ -198,4 +197,3 @@ const char* ROCDECAPI rocDecGetErrorNameHost(rocDecStatus rocdec_status) {
             return "UNKNOWN_ERROR";
     }
 }
-//} //namespace rocdecodehost
